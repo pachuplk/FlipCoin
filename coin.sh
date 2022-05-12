1539db68dd555340772ff9f8af429fd2b422c642
pachuplk <vanguardplk@gmail.com>
2022-05-13 01:19:03 +0530

    flipcoin

---
+++ coin.sh
@@ -0,0 +1,18 @@
+
+#!/bin/sh
+head=1
+tail=0
+headCount=0
+tailCount=0
+for ((i=0; i<10; i++))
+do
+	randomnumber=$((RANDOM%2))
+	if [ $randomnumber -eq $head ]
+	then
+		((headCount++))
+	else
+		((tailCount++))
+	fi
+done
+echo "Number of times Head Won :" $headCount
+echo "Number of times Tail Won :" $tailCount
